4c0709f22e61e6ae16d2a8380a976685b9f89a50
Christopher George <cmgeorge@andrew.cmu.edu>
2020-05-01 19:53:18 -0400

    added pretrained model script

---
+++ run.sh
@@ -0,0 +1,8 @@
+pip3 install gdown
+mkdir ./pretrained_models/
+cd pretrained_models/
+gdown https://drive.google.com/uc?id=1Yl5Crwy08PavXpR3YPEuo_mNZ3pDIM8y
+cd ..
+git clone https://github.com/rois-codh/kmnist.git
+cd kmnist
+python3 download_data.py

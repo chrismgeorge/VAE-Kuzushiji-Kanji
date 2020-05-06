2f1a8dffc88329090c3fac7495feef8781d12e4c
Christopher George <cmgeorge@andrew.cmu.edu>
2020-05-06 13:27:16 -0400

    updated run

--- run.sh
+++ run.sh
@@ -4,11 +4,9 @@ cd pretrained_models/
 gdown https://drive.google.com/uc?id=1Yl5Crwy08PavXpR3YPEuo_mNZ3pDIM8y
 cd ..
 git clone https://github.com/rois-codh/kmnist.git
-cd kmnist
-tar -xvf kkanji.tar
-cd ..
 mv ./download_data.py ./kmnist/
 cd kmnist
 python3 download_data.py
+tar -xvf kkanji.tar
 cd ..
 python3 remove_images.py
